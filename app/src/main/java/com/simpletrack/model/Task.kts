c2e3220c7e81ca9f2e2498593926b27bdf584317
Katharina Aschauer <katharina.aschauer@infoniqa.com>
2021-04-14 09:16:04 +0200

    ST-002 first unit test [AK,KT]

---
+++ app/src/main/java/com/simpletrack/model/Task.kts
@@ -0,0 +1,18 @@
+import java.time
+import java.util.*
+
+class Task{
+    val start: Date
+            get
+            private set
+    val stop: Date
+            get
+            private set
+
+    fun startTime(): void
+
+    fun stopTime(): float
+
+
+
+}
--- app/src/test/java/com/simpletrack/ExampleUnitTest.kt
+++ app/src/test/java/com/simpletrack/ExampleUnitTest.kt
@@ -4,6 +4,8 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+import Task.kt
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,18 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun timeCount_isCorrect(){
+        val task : Task
+
+        task.start()
+        sleep(10)
+        val retval = task.stop()
+
+        assertEquals(retval, 10)
+
+
     }
 }
