0209eaa2df59cf6ea12427ad7d4d9b6234b30134
phil3pi <phil3pi@outlook.com>
2021-04-21 09:02:14 +0200

    ST-008 fixed ktlin errors [CK,SC,SP]

--- app/src/main/java/com/simpletrack/MainActivity.kt
+++ app/src/main/java/com/simpletrack/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.simpletrack
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.simpletrack.view.HomeFragment
@@ -29,19 +25,19 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.navigation_home->setCurrentFragment(homeFragment)
-                R.id.navigation_timer->setCurrentFragment(timerFragment)
-                R.id.navigation_viewList->setCurrentFragment(viewListFragment)
-                R.id.navigation_settings->setCurrentFragment(settingsFragment)
+            when (it.itemId) {
+                R.id.navigation_home -> setCurrentFragment(homeFragment)
+                R.id.navigation_timer -> setCurrentFragment(timerFragment)
+                R.id.navigation_viewList -> setCurrentFragment(viewListFragment)
+                R.id.navigation_settings -> setCurrentFragment(settingsFragment)
             }
             true
         }
     }
 
-    private fun setCurrentFragment(fragment: Fragment)=
+    private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment,fragment)
+            replace(R.id.flFragment, fragment)
             commit()
         }
 }
