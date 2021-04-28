d50b98dac4843513a722951c8476b6a7a6d5b7d0
phil3pi <phil3pi@outlook.com>
2021-04-28 10:29:46 +0200

    ST-010 added functionality of adding tasks to list [SP,SC,KT]

--- app/src/main/java/com/simpletrack/MainActivity.kt
+++ app/src/main/java/com/simpletrack/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.simpletrack.model.Task
 import com.simpletrack.view.HomeFragment
 import com.simpletrack.view.SettingsFragment
 import com.simpletrack.view.TimerFragment
 import com.simpletrack.view.ViewListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val taskList = ArrayList<Task>()
+        var currentTask: Task? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
