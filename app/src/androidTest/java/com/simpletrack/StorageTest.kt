39760d1ec569451291f927a9b3eb075b18736498
T <tomker31@gmx.at>
2021-05-05 11:01:50 +0200

    ST-012 added new tests [SP, KT]

--- app/src/androidTest/java/com/simpletrack/StorageTest.kt
+++ app/src/androidTest/java/com/simpletrack/StorageTest.kt
@@ -4,16 +4,20 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.simpletrack.model.Task
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.time.LocalDateTime
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class StorageTest {
     @Test
     fun storeData() {
-        ActivityScenario.launch(MainActivity::class.java)
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
         val storage = MainActivity.storage
         val success = storage.storeData(
             arrayListOf(
@@ -24,12 +28,13 @@ class StorageTest {
                 Task(LocalDateTime.of(2020, 3, 3, 10, 10, 45), LocalDateTime.of(2020, 5, 7, 12, 34, 50), "LOCKDOWNING")
             )
         )
+        scenario.close()
         assert(success)
     }
 
     @Test
     fun loadData() {
-        ActivityScenario.launch(MainActivity::class.java)
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
         val storage = MainActivity.storage
 
         val taskList = arrayListOf(
@@ -46,6 +51,22 @@ class StorageTest {
 
         val loadedData = storage.loadData()
 
-        assert(taskList.size == loadedData.size)
+        scenario.close()
+        assertEquals(taskList.size, loadedData.size)
+    }
+
+
+    @Test
+    fun TestStorageIntegration() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        val storage = MainActivity.storage
+        onView(withId(R.id.startButton)).perform(click())
+        onView(withId(R.id.stopButton)).perform(click())
+
+        val loadedData = storage.loadData()
+
+        scenario.close()
+        assertEquals(1, loadedData.size)
+
     }
 }
