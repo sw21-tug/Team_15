31eb45ea08e9ddfbdfdd3ce37280b6749400390d
chairwinker <kai-czerwinka@hotmail.com>
2021-05-05 08:26:13 +0200

    ST-009 russian translation, changing languange not working yet [AK, CK]

--- app/src/main/java/com/simpletrack/MainActivity.kt
+++ app/src/main/java/com/simpletrack/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.simpletrack
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -8,6 +12,7 @@ import com.simpletrack.view.HomeFragment
 import com.simpletrack.view.SettingsFragment
 import com.simpletrack.view.TimerFragment
 import com.simpletrack.view.ViewListFragment
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,4 +45,45 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.flFragment, fragment)
             commit()
         }
+
+    fun setLanguage(context: Context, language: String): ContextWrapper {
+        var mContext = context
+
+        val localeLang = language.split("_".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val locale: Locale
+        if (localeLang.size > 1)
+            locale = Locale(localeLang[0], localeLang[1])
+        else
+            locale = Locale(localeLang[0])
+
+        val res = mContext.resources
+        val configuration = res.configuration
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val localeList = LocaleList(locale)
+            LocaleList.setDefault(localeList)
+            configuration.setLocales(localeList)
+            mContext = mContext.createConfigurationContext(configuration)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLocale(locale)
+            mContext = mContext.createConfigurationContext(configuration)
+        } else {
+            configuration.locale = locale
+            res.updateConfiguration(configuration, res.getDisplayMetrics())
+        }
+
+        return ContextWrapper(mContext)
+    }
+
+    //    For Language Changing
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(
+            newBase?.let {
+                setLanguage(
+                    it,
+                    "rus"
+                )
+            }
+        )
+    }
 }
--- app/src/main/java/com/simpletrack/view/SettingsFragment.kt
+++ app/src/main/java/com/simpletrack/view/SettingsFragment.kt
@@ -1,9 +1,13 @@
 package com.simpletrack.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.simpletrack.R
@@ -27,7 +31,26 @@ class SettingsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        // https://stackoverflow.com/questions/58908518/not-able-to-change-language-locale-in-kotlin
+        // https://stackoverflow.com/questions/50785840/change-locale-programmatically-in-kotlin
+        // https://stackoverflow.com/questions/59713615/translation-of-ui-android-in-kotlin
         // TODO: Use the ViewModel
+        requireView().findViewById<TextView>(R.id.currentLang).text = super.getResources().configuration.locale.toString()
+
+        view?.findViewById<Button>(R.id.lang_rus)?.setOnClickListener {
+            Handler().post {
+                val intent = activity?.intent
+                intent?.flags =
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION
+                activity?.overridePendingTransition(0, 0)
+                activity?.finish()
+
+                activity?.overridePendingTransition(0, 0)
+                startActivity(intent)
+            }
+            viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        }
     }
 }
