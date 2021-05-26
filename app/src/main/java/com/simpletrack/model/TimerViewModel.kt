0bbb496dc12840daf19659d7c409e36184a4c551
T <tomker31@gmx.at>
2021-05-26 12:14:03 +0200

    ST-018 fixed a error itroduced during merging [CK, KT]

--- app/src/main/java/com/simpletrack/model/TimerViewModel.kt
+++ app/src/main/java/com/simpletrack/model/TimerViewModel.kt
@@ -1,6 +1,7 @@
 package com.simpletrack.model
 
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.simpletrack.MainActivity
 import com.simpletrack.R
@@ -57,7 +58,12 @@ class TimerViewModel(val timerFragment: TimerFragment) {
 
     fun stopTimer() {
         MainActivity.currentTask!!.stopTime()
-        MainActivity.currentTask!!.name = timerFragment.spinner.selectedItem.toString()
+        val taskTypes = timerFragment.resources.getStringArray(R.array.TaskTypes)
+        if (timerFragment.spinner.selectedItem == taskTypes[taskTypes.size - 1]) {
+            MainActivity.currentTask!!.name = timerFragment.requireView().findViewById<EditText>(R.id.customTasknameInput).text.toString()
+        } else {
+            MainActivity.currentTask!!.name = timerFragment.spinner.selectedItem.toString()
+        }
         if (MainActivity.currentTask!!.isStopped()) {
             MainActivity.taskList.add(MainActivity.currentTask!!)
             MainActivity.storage.storeData(MainActivity.taskList)
