1be24a333fb1601b3a65e46b4fae9d3fecda3621
chairwinker <kai-czerwinka@hotmail.com>
2021-06-02 11:51:14 +0200

    ST-020 fix ktlint [CK,AK,SC]

--- app/src/main/java/com/simpletrack/view/TimerFragment.kt
+++ app/src/main/java/com/simpletrack/view/TimerFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.simpletrack.MainActivity
 import com.simpletrack.R
-import com.simpletrack.model.TimerButtonState
 import com.simpletrack.model.TimerViewModel
 
 class TimerFragment : Fragment() {
